7bc8d9250f0580e1092195a8467eece23e038cc4
BooookStore <booookstore@icloud.com>
2017-11-03 16:43:08 +0900

    ADD:カスタマーサービスクラスにメソッドを追加

--- src/main/java/com/packt/webstore/service/CustomerService.java
+++ src/main/java/com/packt/webstore/service/CustomerService.java
@@ -3,6 +3,7 @@ package com.packt.webstore.service;
 import com.packt.webstore.domain.Customer;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * 顧客情報に対する操作を定義
@@ -15,5 +16,9 @@ public interface CustomerService {
      * @return 全ての顧客情報
      */
     List<Customer> getAllCustomers();
+
+    List<Customer> getCustomersByFilter(Map<String, List<String>> filter);
+
+    Customer getCustomerById(String id);
 
 }
