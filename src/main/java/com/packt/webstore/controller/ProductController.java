10080495247aef5856203d79ae4d22b6aff76fec
bookstore <booookstore@icloud.com>
2018-01-28 17:47:48 +0900

    feat : 製品画像アップロード機能を追加

--- src/main/java/com/packt/webstore/controller/ProductController.java
+++ src/main/java/com/packt/webstore/controller/ProductController.java
@@ -8,9 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.File;
 import java.util.List;
 import java.util.Map;
 
@@ -64,7 +66,18 @@ public class ProductController {
     }
 
     @RequestMapping(value = "/product/add", method = RequestMethod.POST)
-    public String processAddNewProductForm(@ModelAttribute("newProduct") Product product) {
+    public String processAddNewProductForm(@ModelAttribute("newProduct") Product product, HttpServletRequest request) {
+        MultipartFile productImage = product.getProductImage();
+        String rootDirectory = request.getSession().getServletContext().getRealPath("/");
+        if (productImage != null && !productImage.isEmpty()) {
+            try {
+                System.out.println(rootDirectory + "resources/images/" + product.getProductId() + ".jpg");
+                productImage.transferTo(new File(rootDirectory + "resources/images/" + product.getProductId() + ".jpg"));
+            } catch (Exception e) {
+                throw new RuntimeException("Product Image Saving Failed.");
+            }
+        }
+
         this.productService.addProduct(product);
         return "redirect:/market/products";
     }
